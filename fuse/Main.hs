fefe848c226bebb34c4ad78a27f1f2f593c1e3d8
Matej Kollar <208115@mail.muni.cz>
2021-09-18 18:22:03 +0100

    Dummy ro FS

--- fuse/Main.hs
+++ fuse/Main.hs
@@ -1,17 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import GHC.Real (fromIntegral)
+
 import Control.Applicative (pure)
-import Data.Function (($), (.))
+import Control.Monad ((>>=))
 import Data.Either (Either(Left, Right))
+import Data.Function (($), (.))
+import Data.Functor ((<$>), fmap)
+import Data.IORef (IORef, newIORef, readIORef)
 import Data.List (foldr1)
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Monoid ((<>))
 import System.IO (FilePath, IO, putStrLn)
 
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import System.Fuse
     ( EntryType(Directory, RegularFile)
     , Errno
     , FileStat(FileStat)
     , FuseContext
     , FuseOperations
+    , OpenFileFlags
+    , OpenMode(ReadOnly)
     , defaultExceptionHandler
     , defaultFuseOps
     , eNOENT
@@ -20,8 +34,11 @@ import System.Fuse
     , fuseCtxUserID
     , fuseGetFileStat
     , fuseMain
+    , fuseOpen
     , fuseOpenDirectory
+    , fuseRead
     , fuseReadDirectory
+    , fuseRelease
     , getFuseContext
     , statAccessTime
     , statBlocks
@@ -36,13 +53,17 @@ import System.Fuse
     , statStatusChangeTime
     )
 import System.Posix.Files (unionFileModes, ownerReadMode, ownerExecuteMode, groupReadMode, groupExecuteMode, otherReadMode, otherExecuteMode)
+import System.Posix.Types (ByteCount, FileOffset)
 
 
-data State = State
+type File = ByteString
+type FS = Map FilePath File
+newtype State = State (IORef FS)
 
 data Handle = Handle
 
 type FuseOut a = IO (Either Errno a)
+type FuseRet = IO Errno
 
 fail :: Errno -> FuseOut a
 fail = pure . Left
@@ -51,20 +72,7 @@ ok :: a -> FuseOut a
 ok = pure . Right
 
 mkState :: IO State
-mkState = pure State
-
-main :: IO ()
-main = do
-    putStrLn "Starting"
-    st <- mkState
-    fuseMain (flatFuseOps st) defaultExceptionHandler
-
-flatFuseOps :: State -> FuseOperations Handle
-flatFuseOps _ = defaultFuseOps
-    { fuseGetFileStat = flatGetFileStat
-    , fuseOpenDirectory = flatOpenDirectory
-    , fuseReadDirectory = flatReadDirectory
-    }
+mkState = State <$> newIORef (Map.fromList [("a", "xa\n"), ("b", "xb\n")])
 
 dirStat :: FuseContext -> FileStat
 dirStat ctx = FileStat
@@ -88,19 +96,17 @@ dirStat ctx = FileStat
     , statStatusChangeTime = 0
     }
 
-fileStat :: FuseContext -> FileStat
-fileStat ctx = FileStat
+fileStat :: File -> FuseContext -> FileStat
+fileStat f ctx = FileStat
     { statEntryType = RegularFile
     , statFileMode = foldr1 unionFileModes
         [ ownerReadMode
-        , groupReadMode
-        , otherReadMode
         ]
     , statLinkCount = 1
     , statFileOwner = fuseCtxUserID ctx
     , statFileGroup = fuseCtxGroupID ctx
     , statSpecialDeviceID = 0
-    , statFileSize = 0
+    , statFileSize = fromIntegral (BS.length f)
     , statBlocks = 1
     , statAccessTime = 0
     , statModificationTime = 0
@@ -108,22 +114,62 @@ fileStat ctx = FileStat
     }
 
 
-flatGetFileStat :: FilePath -> FuseOut FileStat
-flatGetFileStat "/" = do
-    ctx <- getFuseContext
-    ok $ dirStat ctx
-flatGetFileStat _ =
-    fail eNOENT
+flatGetFileStat :: State -> FilePath -> FuseOut FileStat
+flatGetFileStat _ "/" = getFuseContext >>= ok . dirStat
+flatGetFileStat (State st) ('/':fileName) = do
+    fs <- readIORef st
+    putStrLn fileName
+    case Map.lookup fileName fs of
+        Just f -> getFuseContext >>= ok . fileStat f
+        Nothing -> fail eNOENT
+flatGetFileStat _ _ = fail eNOENT
 
-flatReadDirectory :: FilePath -> FuseOut [(FilePath, FileStat)]
-flatReadDirectory "/" = do
-    ctx <- getFuseContext
-    ok
-        [ (".", dirStat ctx)
-        , ("..", dirStat ctx)
-        ]
-flatReadDirectory _ = fail eNOENT
-
-flatOpenDirectory :: FilePath -> IO Errno
+flatOpenDirectory :: FilePath -> FuseRet
 flatOpenDirectory "/" = pure eOK
 flatOpenDirectory _   = pure eNOENT
+
+flatReadDirectory :: State -> FilePath -> FuseOut [(FilePath, FileStat)]
+flatReadDirectory (State st) "/" = do
+    ctx <- getFuseContext
+    fs <- readIORef st
+    ok $
+        [ (".", dirStat ctx)
+        , ("..", dirStat ctx)
+        ] <>
+        fmap (\(n, c) -> (n, fileStat c ctx)) (Map.assocs fs)
+flatReadDirectory _ _ = fail eNOENT
+
+flatFuseOps :: State -> FuseOperations Handle
+flatFuseOps st = defaultFuseOps
+    { fuseGetFileStat = flatGetFileStat st
+    , fuseOpenDirectory = flatOpenDirectory
+    , fuseReadDirectory = flatReadDirectory st
+    , fuseOpen = flatOpen st
+    , fuseRead = flatRead st
+    , fuseRelease = flatClose st
+    }
+
+flatOpen :: State -> FilePath -> OpenMode -> OpenFileFlags -> FuseOut Handle
+flatOpen (State st) ('/':fileName) ReadOnly _flags = do
+    fs <- readIORef st
+    if Map.member fileName fs
+        then ok Handle
+        else fail eNOENT
+flatOpen _ _ _ _ = fail eNOENT
+
+flatRead :: State -> FilePath -> Handle -> ByteCount -> FileOffset -> FuseOut ByteString
+flatRead (State st) ('/':fileName) Handle bc fo = do
+    fs <- readIORef st
+    case Map.lookup fileName fs of
+        Nothing -> fail eNOENT
+        Just f -> ok $ BS.take (fromIntegral bc) (BS.drop (fromIntegral fo) f)
+flatRead _ _ _ _ _ = fail eNOENT
+
+flatClose :: State -> FilePath -> Handle -> IO ()
+flatClose _ _ Handle = pure ()
+
+main :: IO ()
+main = do
+    putStrLn "Starting"
+    st <- mkState
+    fuseMain (flatFuseOps st) defaultExceptionHandler
